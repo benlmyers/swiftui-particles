d4ecc4f45df117bb48c27306496b6567e71e9233
Ben Myers <benl.myers01@gmail.com>
2023-06-25 20:14:51 -0700

    🔨 Here we go again

---
+++ Sources/Particles/ParticleSystem.swift
@@ -0,0 +1,23 @@
+//
+//  ParticleSystem.swift
+//  
+//
+//  Created by Ben Myers on 6/25/23.
+//
+
+import SwiftUI
+
+struct ParticleSystem: View {
+  
+  // MARK: - Views
+  
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct ParticleSystem_Previews: PreviewProvider {
+  static var previews: some View {
+    ParticleSystem()
+  }
+}
