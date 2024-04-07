c2a6cb9e73530acf43f7b8d7f40b6623a76168f7
Benjamin Myers <benl.myers01@gmail.com>
2024-04-07 16:30:04 -0700

    ⚡️ Edge lattice

--- Sources/Particles/API/Entities/Lattice.swift
+++ Sources/Particles/API/Entities/Lattice.swift
@@ -28,6 +28,7 @@ public struct Lattice: Entity, Transparent {
   
   // MARK: - Properties
   
+  private var mode: Mode
   private var customView: AnyView = AnyView(Circle().frame(width: 2.0, height: 2.0))
   private var spawns: [(CGPoint, Color)]
   private var viewSize: CGSize
@@ -37,23 +38,31 @@ public struct Lattice: Entity, Transparent {
   
   /// Creates a new Lattice particle group, which creates a grid of colored particles atop the opaque pixels of a view.
   /// - Parameter spacing: Distance between each particle in the lattice.
+  /// - Parameter edges: The edges to spawn particles on. If empty `[]` (default), the lattice uses normal behavior and overlays entirely across the passed view. If `.all`,
   /// - Parameter anchor: Whether to spawn the lattice of particles relative to the view.
   /// - Parameter view: The view that is used as a source layer to choose where to spawn various colored particles.
   public init<Base>(
-    spacing: Int = 3,
+    _ mode: Mode = .cover,
+    spacing: CGFloat = 3.0,
     anchor: UnitPoint = .center,
     @ViewBuilder view: () -> Base
   ) where Base: View {
-    #if os(iOS)
+    
+#if os(iOS)
     let s = 0.66
-    #else
+#else
     let s = 1.0
-    #endif
+#endif
+    
     guard let viewImage = view().scaleEffect(s).background(Color.black).asImage()?.cgImage, let imgData = viewImage.dataProvider?.data else {
       fatalError("Particles could not convert view to image correctly. (Burst)")
     }
+    
     viewSize = .init(width: viewImage.width / 2, height: viewImage.height / 2)
-    func getPixelColorAt(x: Int, y: Int) -> Color? {
+    
+    var pixelColorCache: [String: Color] = [:]
+    func getPixelColorAt(x: Int, y: Int, useCache: Bool = false) -> Color? {
+      if useCache, let color = pixelColorCache["\(x)_\(y)"] { return color }
       let data: UnsafePointer<UInt8> = CFDataGetBytePtr(imgData)
       let bpr: Int = viewImage.bytesPerRow
       let pixelInfo: Int = (bpr * y*2) + 4 * x*2
@@ -62,20 +71,52 @@ public struct Lattice: Entity, Transparent {
       let b = CGFloat(data[pixelInfo + 2]) / CGFloat(255.0)
       let a = CGFloat(data[pixelInfo + 3]) / CGFloat(255.0)
       let color = Color(red: Double(r), green: Double(g), blue: Double(b), opacity: Double(a))
-      if a == 0 || r + g + b < 0.1 { return nil }
+      if a == 0 || r + g + b < 0.5 { return nil }
+      if useCache { pixelColorCache["\(x)_\(y)"] = color }
       return color
     }
-    var spawns: [(CGPoint, Color)] = []
-    for x in stride(from: 0, to: viewImage.width / 2, by: spacing) {
-      for y in stride(from: 0, to: viewImage.height / 2, by: spacing) {
-        if let color = getPixelColorAt(x: x, y: y) {
-          spawns.append((CGPoint(x: x, y: y), color))
+    
+    self.mode = mode
+    self.spawns = []
+    self.anchor = anchor
+    self.customView = AnyView(Circle().frame(width: 2, height: 2))
+    
+    var newSpawns: [(CGPoint, Color)] = []
+    
+    switch mode {
+    case .cover:
+      for x in stride(from: 0.0, to: CGFloat(viewImage.width) / 2.0, by: spacing) {
+        for y in stride(from: 0.0, to: CGFloat(viewImage.height) / 2.0, by: spacing) {
+          if let color = getPixelColorAt(x: Int(x), y: Int(y)) {
+            newSpawns.append((CGPoint(x: x, y: y), color))
+          }
+        }
+      }
+    case .hug(let array):
+      let flat: [(Edge, CGPoint)] = array.flatMap({ edge in edge.points(size: viewSize, spacing: spacing).map({ point in (edge, point) }) })
+      for (edge, point) in flat {
+        var proxy: CGPoint = point
+        while proxy.x >= 0 && proxy.x <= viewSize.width && proxy.y >= 0 && proxy.y <= viewSize.height {
+          if let color = getPixelColorAt(x: Int(proxy.x), y: Int(proxy.y), useCache: false) {
+            newSpawns.append((CGPoint(x: proxy.x, y: proxy.y), .red))
+            break
+          } else {
+            switch edge {
+            case .top:
+              proxy.y += spacing
+            case .leading:
+              proxy.x += spacing
+            case .bottom:
+              proxy.y -= spacing
+            case .trailing:
+              proxy.x -= spacing
+            }
+          }
         }
       }
     }
-    self.spawns = spawns
-    self.anchor = anchor
-    self.customView = AnyView(Circle().frame(width: 2, height: 2))
+    
+    self.spawns = newSpawns
   }
   
   // MARK: - Body Entity
@@ -94,5 +135,48 @@ public struct Lattice: Entity, Transparent {
     var copy = self
     copy.customView = .init(view())
     return copy
+  }
+  
+  // MARK: - Subtypes
+  
+  /// How ``Lattice`` choose to generate particles.
+  public enum Mode {
+    /// Cover the entire view with particles.
+    case cover
+    /// Hug the outside edges of the view with particles.
+    case hug([Edge])
+  }
+  
+  /// A edge against which Lattice choose to generate particles.
+  public enum Edge {
+    /// The top edge of a view.
+    case top
+    /// The leading edge of a view.
+    case leading
+    /// The bottom edge of a view.
+    case bottom
+    /// The trailing edge of a view.
+    case trailing
+    
+    fileprivate func points(size: CGSize, spacing: CGFloat) -> [CGPoint] {
+      switch self {
+      case .top:
+        return stride(from: 0, to: size.width, by: spacing).map({ .init(x: $0, y: 0.0)})
+      case .leading:
+        return stride(from: 0, to: size.height, by: spacing).map({ .init(x: 0.0, y: $0)})
+      case .bottom:
+        return stride(from: 0, to: size.width, by: spacing).map({ .init(x: $0, y: size.height)})
+      case .trailing:
+        return stride(from: 0, to: size.height, by: spacing).map({ .init(x: size.width, y: $0)})
+      }
+    }
+  }
+}
+
+public extension Array where Element == Lattice.Edge {
+  
+  /// Hugs every edge.
+  static var all : Self {
+    [.top, .leading, .bottom, .trailing]
   }
 }
